33128c4cc00b601ce63223ca1231c023bc06d263
Ollie Tribe <tribe@myself.com>
2019-04-14 15:31:03 +0100

    Add token create w/ jsonwebtoken

--- lib/index.ts
+++ lib/index.ts
@@ -1,24 +1,32 @@
 import * as bcrypt from 'bcrypt'
+import * as jwt from 'jsonwebtoken'
 
 export interface AuthfulOptions {
-  secret: string
+  secret: jwt.Secret
+  expiresIn?: string | number
 }
 
-export interface AuthfulHashParams {
+export interface AuthfulPwdHash {
   plaintext: string
   salt?: string | number
 }
 
-export interface AuthfulCheckParams {
+export interface AuthfulPwdCheck {
   hash: string
   plaintext: string
 }
 
+export interface AuthfulTokenCreate {
+  payload: string | Buffer | object
+}
+
 export default class Authful {
-  secret: string
+  secret: jwt.Secret
+  expiresIn?: string | number
 
   constructor(options: AuthfulOptions) {
     this.secret = options.secret
+    this.expiresIn = options.expiresIn
   }
 
   ready() {
@@ -28,14 +36,22 @@ export default class Authful {
   }
 
   pwd = {
-    async hash(input: AuthfulHashParams) {
+    async hash(input: AuthfulPwdHash) {
       const { plaintext, salt = 10 } = input
       return await bcrypt.hash(plaintext, salt)
     },
 
-    async check(password: AuthfulCheckParams) {
+    async check(password: AuthfulPwdCheck) {
       const { plaintext, hash } = password
       return await bcrypt.compare(plaintext, hash)
+    }
+  }
+
+  token = {
+    create: async (config: AuthfulTokenCreate) => {
+      const { secret, expiresIn = '24hr' } = this
+      const { payload } = config
+      return await jwt.sign(payload, secret, { expiresIn })
     }
   }
 }
--- test/core.spec.ts
+++ test/core.spec.ts
@@ -3,24 +3,32 @@ import Authful from '@lib/index'
 let authful!: Authful
 const secret = 'ffj3493$R#J@$f#$Jo3ofk'
 const plaintext = 'p4$$w@rd'
-const input = { plaintext }
 
 beforeEach(() => {
   authful = new Authful({ secret })
 })
 
-test('new Authful()', () => {
+test('instance', () => {
   expect(authful.ready()).toEqual({ secret })
 })
 
-test('hash()', async () => {
-  const regex = /^\$2[ayb]\$.{56}$/
-  const hash = await authful.pwd.hash(input)
-  expect(hash).toEqual(expect.stringMatching(regex))
+describe('pwd', () => {
+  test('hash', async () => {
+    const regex = /^\$2[ayb]\$.{56}$/
+    const hash = await authful.pwd.hash({ plaintext })
+    expect(hash).toEqual(expect.stringMatching(regex))
+  })
+
+  test('check', async () => {
+    const hash = await authful.pwd.hash({ plaintext })
+    const match = await authful.pwd.check({ hash, plaintext })
+    expect(match).toBeTruthy()
+  })
 })
 
-test('check()', async () => {
-  const hash = await authful.pwd.hash(input)
-  const match = await authful.pwd.check({ hash, plaintext })
-  expect(match).toBeTruthy()
+describe('token', () => {
+  test('create', async () => {
+    const payload = { id: 1 }
+    const token = await authful.token.create({ payload })
+  })
 })
